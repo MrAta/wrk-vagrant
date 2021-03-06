5c6c88aacedbaa7aec38db5ff571d4a29f792366
Ata Fatahi <immrata@gmail.com>
2021-03-06 00:28:11 -0500

    initial commit
    Signed-off-by: Ata Fatahi <immrata@gmail.com>

---
+++ scripts/social-network/read-home-timeline.lua
@@ -0,0 +1,19 @@
+require "socket"
+local time = socket.gettime()*1000
+math.randomseed(time)
+math.random(); math.random(); math.random()
+
+request = function()
+  local user_id = tostring(math.random(1, 962))
+  local start = tostring(math.random(0, 100))
+  local stop = tostring(start + 10)
+
+  local args = "user_id=" .. user_id .. "&start=" .. start .. "&stop=" .. stop
+  local method = "GET"
+  local headers = {}
+  headers["Content-Type"] = "application/x-www-form-urlencoded"
+  -- Since nginx-thrift service is port-forwarded to localhost, we use the localhost address here here:
+  local path = "http://10.0.2.2:8080/wrk2-api/home-timeline/read?" .. args
+  return wrk.format(method, path, headers, nil)
+
+end
